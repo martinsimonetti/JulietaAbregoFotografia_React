4598df249a3bdd0248f00904a24dc9d3d2aad1a6
Martín Simonetti <msimonetti@gmail.com>
2021-09-27 16:24:33 -0300

    Se creó el Footer. Se agregaron las librerías de FontAwesome.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import NavBar from './components/layout/NavBar.js';
+import Footer from './components/layout/Footer.js';
 
 function App() {
   return (
     <div className="Container">
         {/* Componentes de la App */}
         <NavBar></NavBar>
+        <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,44 @@
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faTiktok } from "@fortawesome/free-brands-svg-icons";
+
+const Footer = (props) => {
+    return (
+        <footer className='bg-secondary text-secondary text-center text-lg-start'>
+            <Container className='p-4'>
+                <Row>
+                    <Col lg={6} md={12} className='mb-6 md-0'>
+                        <h5 className='text-secondary align-self-start'>Quiero info sobre...</h5>
+                        <ul className='list-unstyled'>
+                            {/* <li><Link href='/' className='text-secondary text-decoration-none'>Servicios</Link></li>
+                            <li><Link href='/' className='text-secondary text-decoration-none'>Cursos</Link></li>
+                            <li><Link href='/' className='text-secondary text-decoration-none'>Recursos gratuitos</Link></li> */}
+                        </ul>
+                    </Col>
+                    <Col lg={3} className='mb-6 md-0'>
+
+                    </Col>
+                    <Col lg={3} md={12} className='mb-6 md-0'>
+                        <h5 className='text-secondary align-self-start'>Buscame en las redes</h5>
+                        <ul className='list-unstyled'>
+                            <li><a href="https://wa.me/message/LA5VAWG3CCUDF1" class="text-secondary text-decoration-none"><FontAwesomeIcon icon={faWhatsapp} /> 11 3042-2765</a></li>
+                            <li><a href="https://www.instagram.com/julietaabregoph/" class="text-secondary text-decoration-none" target="_blank"><FontAwesomeIcon icon={faInstagram} /> @julietaabregoph</a></li>
+                            <li><a href="https://www.tiktok.com/@julietaabregoph" class="text-secondary text-decoration-none" target="_blank"><FontAwesomeIcon icon={faTiktok} /> @julietaabregoph</a></li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
+            <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+                © 2021 Copyright
+                <p><a className="text-secondary text-decoration-none" href="#">Martín Simonetti</a></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
