cf7308e01adfcddd754a3f7dcbc6d5396d3193c1
Martín Simonetti <msimonetti@gmail.com>
2021-10-10 11:31:53 -0300

    Se creó la Sección1 de Recursos.

---
+++ src/components/layout/Recursos-Seccion1.js
@@ -0,0 +1,64 @@
+import { Container, Row, Col, Card } from "react-bootstrap";
+
+const Seccion1 = (props) => {
+    return (
+        <section>
+            <Container>
+                <Row xs={1} sm={2} md={3} lg={4}>
+                    <Col>
+                        <Card.Img src='https://via.placeholder.com/1024x768.png' alt='' className='my-4' />
+                        <h3 class='text-center'>Título</h3>
+                        <Card.Text className='text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.Unde totam quis enim temporibus
+                            laborum dolores delectus repellendus ea libero, ex at, obcaecati, consequatur voluptas eaque
+                            blanditiis?Facilis mollitia ipsa dolor!</Card.Text>
+                        <div className='text-center'>
+                            <a href='#' className='btn btn-primary'>Descargar</a>
+                        </div>
+                    </Col>
+                    <Col>
+                        <Card.Img src='https://via.placeholder.com/1024x768.png' alt='' className='my-4' />
+                        <h3 class='text-center'>Título</h3>
+                        <Card.Text className='text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.Unde totam quis enim temporibus
+                            laborum dolores delectus repellendus ea libero, ex at, obcaecati, consequatur voluptas eaque
+                            blanditiis?Facilis mollitia ipsa dolor!</Card.Text>
+                        <div className='text-center'>
+                            <a href='#' className='btn btn-primary'>Descargar</a>
+                        </div>
+                    </Col>
+                    <Col>
+                        <Card.Img src='https://via.placeholder.com/1024x768.png' alt='' className='my-4' />
+                        <h3 class='text-center'>Título</h3>
+                        <Card.Text className='text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.Unde totam quis enim temporibus
+                            laborum dolores delectus repellendus ea libero, ex at, obcaecati, consequatur voluptas eaque
+                            blanditiis?Facilis mollitia ipsa dolor!</Card.Text>
+                        <div className='text-center'>
+                            <a href='#' className='btn btn-primary'>Descargar</a>
+                        </div>
+                    </Col>
+                    <Col>
+                        <Card.Img src='https://via.placeholder.com/1024x768.png' alt='' className='my-4' />
+                        <h3 class='text-center'>Título</h3>
+                        <Card.Text className='text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.Unde totam quis enim temporibus
+                            laborum dolores delectus repellendus ea libero, ex at, obcaecati, consequatur voluptas eaque
+                            blanditiis?Facilis mollitia ipsa dolor!</Card.Text>
+                        <div className='text-center'>
+                            <a href='#' className='btn btn-primary'>Descargar</a>
+                        </div>
+                    </Col>
+                    <Col>
+                        <Card.Img src='https://via.placeholder.com/1024x768.png' alt='' className='my-4' />
+                        <h3 class='text-center'>Título</h3>
+                        <Card.Text className='text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.Unde totam quis enim temporibus
+                            laborum dolores delectus repellendus ea libero, ex at, obcaecati, consequatur voluptas eaque
+                            blanditiis?Facilis mollitia ipsa dolor!</Card.Text>
+                        <div className='text-center'>
+                            <a href='#' className='btn btn-primary'>Descargar</a>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+}
+
+export default Seccion1;
